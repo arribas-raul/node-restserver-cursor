b45ce57723b09efd32968d6b9426887e0846f1cd
Raúl <arribas.raul@gmail.com>
2020-08-23 23:00:30 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ if( process.env.NODE_ENV === 'dev'){
      urlDB = 'mongodb://localhost:27017/cafe';
 
 }else{
-     urlDB = 'mongodb+srv://strider:4dYy1IzrJ1P5ar1u@cluster0.fxzal.gcp.mongodb.net/cafe';
+     urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
