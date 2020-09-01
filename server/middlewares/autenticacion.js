fb5a2df0edb48bc59f84aaba28adaebee629f81e
Raúl <arribas.raul@gmail.com>
2020-09-01 18:54:54 +0200

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -26,6 +26,29 @@ let verificaToken = ( req, res, next ) =>{
 };
 
 // ============================
+// Verificar Token para imagen
+// ============================
+let verificaTokenImg = (req, res, next) => {
+     let token = req.query.token;
+
+     jwt.verify(token, process.env.SEED, (err, decoded) =>{
+
+          if( err ){
+               return res.status(401).json({
+                    ok: false,
+                    err: {
+                         message: 'Token no válido'
+                    }
+               });
+          }
+
+          req.usuario = decoded.usuario;
+
+          next();
+     });
+}
+
+// ============================
 // Verificar AdminRole
 // ============================
 let verificaAdminRole = ( req, res, next ) =>{
@@ -47,5 +70,6 @@ let verificaAdminRole = ( req, res, next ) =>{
 
 module.exports = {
      verificaToken,
+     verificaTokenImg,
      verificaAdminRole
 }
